47e213ef195b3801663ba904b9ab40c4a8b243b8
oguzbozak <43357048+oguzbozak@users.noreply.github.com>
2022-05-19 01:44:28 +0300

    first commit

---
+++ src/test/java/com/mikroservicesproject/springbootmicroservices1course/SpringBootMicroservices1CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikroservicesproject.springbootmicroservices1course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservices1CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
