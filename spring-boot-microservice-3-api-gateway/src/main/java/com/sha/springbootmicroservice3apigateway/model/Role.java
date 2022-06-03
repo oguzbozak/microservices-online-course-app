190504ac5c45e2db630c3cd17dd046403ca34548
oguzbozak <43357048+oguzbozak@users.noreply.github.com>
2022-06-04 00:55:12 +0300

    Api Gateway

---
+++ spring-boot-microservice-3-api-gateway/src/main/java/com/sha/springbootmicroservice3apigateway/model/Role.java
@@ -0,0 +1,8 @@
+package com.sha.springbootmicroservice3apigateway.model;
+
+
+public enum Role
+{
+    USER,
+    ADMIN
+}
---
+++ spring-boot-microservice-3-api-gateway/src/main/java/com/sha/springbootmicroservice3apigateway/request/CourseServiceRequest.java
@@ -0,0 +1,23 @@
+package com.sha.springbootmicroservice3apigateway.request;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(value = "course-service"//Name of course-service application
+        , path = "/api/course"//Pre-path for course-service
+        //,url = "${course.service.url}" dont need because we use eureka
+        ,configuration = FeignConfiguration.class
+)
+public interface CourseServiceRequest
+{
+    @PostMapping///api/course
+    Object saveCourse(@RequestBody Object requestBody);
+
+    @DeleteMapping("{courseId}")//api/course/{courseId}
+    void deleteCourse(@PathVariable("courseId") Long courseId);
+
+    @GetMapping//api/course
+    List<Object> getAllCourses();
+}
