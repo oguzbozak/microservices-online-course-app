a3d0270b465dfdc18e272488172f7aa6575ca7ef
oguzbozak <43357048+oguzbozak@users.noreply.github.com>
2022-06-04 00:55:53 +0300

    Eureka Service

---
+++ spring-boot-microservices-4-eureka/src/test/java/com/microservicesproject/springbootmicroservices4eureka/SpringBootMicroservices4EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesproject.springbootmicroservices4eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservices4EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
